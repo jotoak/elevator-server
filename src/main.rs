5a2d47e9536e80685f80421afcc85b8b9ae3ff24
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-12-30 20:54:08 +0100

    Added set_direction to ElevatorInterface

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,17 @@ extern "C" {
     fn comedi_data_read(it: *const comedi_t, subd: libc::c_uint, chan: libc::c_uint, range: libc::c_uint, aref: libc::c_uint, data: *mut libc::c_uint) -> libc::c_int;
 }
 
+pub enum ElevatorDirection{
+    Up,
+    Down,
+    Stop,
+}
+
 pub struct ElevatorInterface(*const comedi_t);
 
 impl ElevatorInterface {
+    const MOTOR_SPEED: u32 = 2800;
+    
     fn open(interface_name: &str) -> Result<Self, ()> {
         unsafe {
             let comedi = comedi_open(CString::new(interface_name).unwrap().as_ptr());
@@ -29,6 +37,25 @@ impl ElevatorInterface {
             }
         }
     }
+
+    fn set_direction(&self, dir: ElevatorDirection) {
+        unsafe {
+            match dir {
+                ElevatorDirection::Up => {
+                    comedi_dio_write(self.0, channel::MOTORDIR >> 8, channel::MOTORDIR & 0xff, 0);
+                    comedi_data_write(self.0, channel::MOTOR >> 8, channel::MOTOR & 0xff, 0, 0, Self::MOTOR_SPEED);
+                },
+                ElevatorDirection::Down => {
+                    comedi_dio_write(self.0, channel::MOTORDIR >> 8, channel::MOTORDIR & 0xff, 1);
+                    comedi_data_write(self.0, channel::MOTOR >> 8, channel::MOTOR & 0xff, 0, 0, Self::MOTOR_SPEED);
+                },
+                ElevatorDirection::Stop => {
+                    comedi_data_write(self.0, channel::MOTOR >> 8, channel::MOTOR & 0xff, 0, 0, 0);
+                },
+            }
+        }
+    }
+        
 }
 
 fn main() {
