fcf8eba81360948dd0c04269b1805461364ebb90
Kjetil Kjeka <kjetilkjeka@gmail.com>
2017-12-30 20:17:23 +0100

    Implemented opening of elevator interface

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,25 @@ extern "C" {
     fn comedi_data_read(it: *const comedi_t, subd: libc::c_uint, chan: libc::c_uint, range: libc::c_uint, aref: libc::c_uint, data: *mut libc::c_uint) -> libc::c_int;
 }
 
+pub struct ElevatorInterface(*const comedi_t);
+
+impl ElevatorInterface {
+    fn open(interface_name: &str) -> Result<Self, ()> {
+        unsafe {
+            let comedi = comedi_open(CString::new(interface_name).unwrap().as_ptr());
+            if comedi.is_null() {
+                Err(())
+            } else {
+                Ok(ElevatorInterface(comedi))
+            }
+        }
+    }
+}
+
+fn main() {
+    let interface = ElevatorInterface::open("/dev/comedi0").unwrap();
+}
+
 #[cfg(test)]
 mod tests {
     use *;
