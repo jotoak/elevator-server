8d3f1ebee810d85d13843c8dd466d80a6bf74362
Anders Petersen <klasbo@gmail.com>
2019-03-13 16:38:55 +0100

    Move build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+mkdir -p target
+gcc -std=gnu11 -Wall -c src/elev.c -o target/elev.o;
+gcc -std=gnu11 -Wall -c src/io.c -o target/io.o;
+dmd -w -g -oftarget/ElevatorServer src/elevatorserver.d target/elev.o target/io.o -L-lcomedi -L-lm
